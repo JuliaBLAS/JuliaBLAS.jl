943370de2efeb5a5971aa95f2a668c31875a9fba
YingboMa <mayingbo5@gmail.com>
2017-12-14 00:16:44 -0800

    Clean up & add Float32 support

--- src/smallmatrix.jl
+++ src/smallmatrix.jl
@@ -1,36 +1,53 @@
 using SIMD
-import Base:A_mul_B!
 
-@generated function A_mul_B!(::Type{Val{S}}, C::StridedMatrix{T}, A::StridedMatrix{T}, B::StridedMatrix{T}) where {T <: Float64, S}
-    VL = 4
-    VT = Vec{VL, Float64}
+@generated function _mul!(::Type{Val{S}}, C::StridedMatrix{T}, A::StridedMatrix{T},
+                          B::StridedMatrix{T}, ::Type{Val{G}} = Val{false},
+                          α::T = zero(T), β::T = zero(T)) where {T <: LinAlg.BlasReal, S, G}
+    # AVX256
+    VL = (256÷8)÷sizeof(T)
+    VT = Vec{VL, T}
     N, r  = divrem(S, VL)
-    @assert r == 0 "Only works for 4N × 4N square matrix!"
+    @assert r == 0 "Only works for $(VL)N × $(VL)N square matrix!"
     outerex = Expr( :macrocall, Symbol("@inbounds"), :LineNumberNode, Expr(:block) )
     ex = outerex.args[3]
+    # prefetch Ai0...
     for i in 0:N-1
-        push!( ex.args, Expr(:(=), Symbol(:A, i), :(vload($VT, pointer(A, 1+$VL*$i)))) )
+        push!( ex.args, :($(Symbol(:A, i)) = vload($VT, pointer(A, 1+$VL*$i))) )
     end
 
+    # main loop
     push!( ex.args, Expr(:for, :(i = 1:S), Expr(:block)) )
     mainloop = ex.args[end].args[2].args
     push!( mainloop, :(Bi = B[1, i]) )
+    # take advantage of the prefetch & initialize Mi... which will be later stored in the C matrix
     for i in 0:N-1
-        push!( mainloop, Expr(:(=), Symbol(:Mi, i), Expr(:call, :*, :Bi, Symbol(:A, i)) ) )
+        push!( mainloop, :($(Symbol(:Mi, i)) = Bi * $(Symbol(:A, i))) )
     end
 
     push!( mainloop, Expr(:for, :(u = 2:S), Expr(:block)) )
     innerloop = mainloop[end].args[2].args
+    # enter the inner loop
+    # update Mi... by the rest of Bi column
     push!( innerloop, :(Bu = B[u, i]), :(offset = (u-1)*S) )
+    # separate loads from calculation for better cache locality
     for i in 0:N-1
-        push!( innerloop, Expr( :(=),  Symbol(:Au, i), :(vload($VT, pointer(A, 1+$VL*$i + offset))) ) )
-        push!( innerloop, Expr( :(+=), Symbol(:Mi, i), Expr(:call, :*, :Bu, Symbol(:Au, i)) ) )
+        push!( innerloop, :($(Symbol(:Au, i)) = vload($VT, pointer(A, 1+$VL*$i + offset))) )
+    end
+    for i in 0:N-1
+        push!( innerloop, :($(Symbol(:Mi, i)) += Bu * $(Symbol(:Au, i))) )
     end
 
     push!( mainloop, :(offset = (i-1)*S) )
+    # load Ci column if necessary
+    if G
+        for i in 0:N-1
+            push!( mainloop, :($(Symbol(:Ci, i)) = vload($VT, pointer(C, 1+$VL*$i + offset))))
+            push!( mainloop, :($(Symbol(:Mi, i)) = α * $(Symbol(:Mi, i)) + β * $(Symbol(:Ci, i))))
+        end
+    end
+    # store Mi... in the Ci column
     for i in 0:N-1
-        push!( mainloop, Expr(:call, :vstore, Symbol(:Mi, i), :(pointer(C, 1 + $VL * $i + offset)) ) )
+        push!( mainloop, :(vstore($(Symbol(:Mi, i)), pointer(C, 1 + $VL * $i + offset))) )
     end
     outerex
 end
-
--- test/smallmatrix.jl
+++ test/smallmatrix.jl
@@ -1,13 +1,16 @@
-using JuliaBLAS
+using JuliaBLAS, Test
 
 @testset "Small Matrix Multiplication" begin
-    for i in 4:4:52
-        A = rand(i, i)
-        B = rand(i, i)
-        C = similar(A)
-        A_mul_B!(Val{i}, C, A, B)
-        @test C ≈ A*B
+    for T in (Float64, Float32)
+        for i in 8:8:8*15
+            A = rand(T, i, i)
+            B = rand(T, i, i)
+            C = copy(A)
+            JuliaBLAS._mul!(Val{i}, C, A, B, Val{true}, T(2), T(3))
+            @test C ≈ 2*A*B + 3*A
+            JuliaBLAS._mul!(Val{i}, C, A, B)
+            @test C ≈ A*B
+        end
+        @test_throws AssertionError JuliaBLAS._mul!(Val{5}, rand(T, 5, 5), rand(T, 5, 5), rand(T, 5, 5))
     end
 end
-
-@test_throws AssertionError A_mul_B!(Val{5}, rand(5,5), rand(5,5), rand(5,5))
