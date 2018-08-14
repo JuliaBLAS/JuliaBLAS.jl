f7e70313adb6dfa1154b0cd24c82e7b0caeaab56
YingboMa <mayingbo5@gmail.com>
2018-08-14 13:03:46 -0400

    Add basic tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,13 @@
-using JuliaBLAS, Test
+using JuliaBLAS, Test, LinearAlgebra
 
-include("kernel.jl")
+@testset "Matrix Multiplication Tests" begin
+    siz = (3, 5, 9, 13, 32, 200)
+    for m in siz, n in siz, k in siz
+        C = rand(m, n)
+        A = rand(m, k)
+        B = rand(k, n)
+        ABC = A*B + C
+        mymul!(C, A, B)
+        @test C ≈ ABC
+    end
+end
