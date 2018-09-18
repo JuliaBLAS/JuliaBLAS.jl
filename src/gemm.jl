a48f8fd734ee322d23beec0a767f9a1a9506ac13
YingboMa <mayingbo5@gmail.com>
2018-09-17 23:02:43 -0400

    Add global const buffers

--- src/gemm.jl
+++ src/gemm.jl
@@ -3,8 +3,6 @@ using SIMD
 import Base.Cartesian: @nexprs
 using StaticArrays: MMatrix
 
-@inline micro_ker!(blk, args...) = blk.generic ? _generic_micro_ker!(blk,args...) : _simd_micro_ker!(blk,args...)
-
 struct Block{T1,T2,T3,T4}
     Ac::T1
     Bc::T2
@@ -23,17 +21,24 @@ struct Block{T1,T2,T3,T4}
     inc2C::Int
     generic::Bool
 end
+
+const Ac = Vector{UInt8}(undef, 2350080)
+const Bc = Vector{UInt8}(undef, 6266880)
+const AB = Vector{UInt8}(undef, 12*4*8)
+
 function Block(A::X, B::W, C::Z, generic) where {X, W, Z}
-    mr=8; nr=6
+    global Ac, Bc, AB
+    mr=12; nr=4
     m, n = size(C)
-    mc = min(512, mr*cld(m,mr))
-    nc = min(516, nr*cld(n,nr))
-    kc = min(1024, size(A,2))
+    mc = 72
+    nc = 192
+    kc = 4080
     T = promote_type(eltype(X), eltype(W), eltype(Z))
-    Ac = Matrix{T}(undef, mr, mc*kc÷mr)
-    Bc = Matrix{T}(undef, nr, kc*nc÷nr)
-    AB = zero(MMatrix{mr, nr , T})
-    Block{typeof(Ac),typeof(Bc),typeof(AB),typeof(C)}(Ac, Bc, AB, C, mc, kc, nc, mr, nr,
+    siz = sizeof(T)
+    _Ac = unsafe_wrap(Array, Ptr{T}(pointer(Ac)), length(Ac)÷siz)
+    _Bc = unsafe_wrap(Array, Ptr{T}(pointer(Bc)), length(Bc)÷siz)
+    _AB = unsafe_wrap(Array, Ptr{T}(pointer(AB)), length(AB)÷siz)
+    Block{typeof(_Ac),typeof(_Bc),typeof(_AB),typeof(C)}(_Ac, _Bc, _AB, C, mc, kc, nc, mr, nr,
                                                       strides(A)..., strides(B)..., strides(C)...,
                                                       generic)
 end
@@ -183,43 +188,77 @@ end
     return nothing
 end
 
-function kernel_quote(T1, MR, NR, loadC)
-    quote
-        @inbounds begin
-            pA, pAB = pointer(blk.Ac), pointer(blk.AB)
-            T = eltype($T1); siz = sizeof(T)
-            VT = Vec{$MR, T}
-            if $loadC
-                pC = pointer(blk.C)
-                @nexprs $NR i -> ab_i = vload(VT, pC + (offsetC+(i-1)*blk.inc2C)siz)
-            else
-                @nexprs $NR i -> ab_i = zero(VT)
-            end
-            for k in 1:kc
-                a1 = vload(VT, pA + (offsetA+(k-1)blk.mr)siz)
-                @nexprs $NR i -> begin
-                    b_i = VT(blk.Bc[offsetB+(k-1)blk.nr+i])
-                    ab_i = muladd(a1, b_i, ab_i)
-                end
-            end
-            if $loadC
-                @nexprs $NR i -> vstore(ab_i, pC + (offsetC+(i-1)*blk.inc2C)siz)
-            else
-                @nexprs $NR i -> vstore(ab_i, pAB + (i-1)blk.mr*siz)
-            end
-            return nothing
-        end
-    end
-end
-
-@generated function _simd_micro_ker!(blk::Block{T1,T2,T3,T4}, kc::Int,
+@inline function micro_ker!(blk::Block{T1,T2,T3,T4}, kc::Int,
                                      offsetA::Int, offsetB::Int, offsetC::Int,
                                      ::Val{loadC}) where {T1,T2,T3,T4,loadC}
-    expr = kernel_quote(T1, 8, 6, loadC)
-    quote
-        $(Expr(:meta, :inline))
-        @assert blk.mr == 8 && blk.nr == 6
-        $expr
+    #expr = kernel_quote(T1, 8, 6, loadC)
+    #quote
+    #    $(Expr(:meta, :inline))
+    #    @assert blk.mr == 8 && blk.nr == 6
+    #    $expr
+    #end
+    @inbounds begin
+        if !blk.generic
+            pA, pAB = pointer(blk.Ac), pointer(blk.AB)
+            #T = eltype($T1); siz = sizeof(T)
+            siz = Float64 |> sizeof
+            #VT = Vec{8, Float64}
+            VT = Vec{4, Float64}
+            if loadC
+                pC = pointer(blk.C)
+                #@nexprs 6 i -> ab_i = vload(VT, pC + (offsetC+(i-1)*blk.inc2C)siz)
+                @nexprs 4 i -> begin
+                    ab_i_1 = vload(VT, pC + (offsetC+(i-1)*blk.inc2C  )siz)
+                    ab_i_2 = vload(VT, pC + (offsetC+(i-1)*blk.inc2C+4)siz)
+                    ab_i_3 = vload(VT, pC + (offsetC+(i-1)*blk.inc2C+8)siz)
+                end
+            else
+                @nexprs 4 i -> begin
+                    ab_i_1 = zero(VT)
+                    ab_i_2 = zero(VT)
+                    ab_i_3 = zero(VT)
+                end
+            end
+            for k in 1:kc
+                a1 = vload(VT, pA + (offsetA+blk.mr*(k-1))siz)
+                a2 = vload(VT, pA + (offsetA+blk.mr*(k-1)+4)siz)
+                a3 = vload(VT, pA + (offsetA+blk.mr*(k-1)+8)siz)
+                @nexprs 4 i -> begin
+                    b_i = VT(blk.Bc[offsetB+(k-1)blk.nr+i])
+                    ab_i_1 = muladd(a1, b_i, ab_i_1)
+                    ab_i_2 = muladd(a2, b_i, ab_i_2)
+                    ab_i_3 = muladd(a3, b_i, ab_i_3)
+                end
+            end
+            if loadC
+                @nexprs 4 i -> begin
+                    vstore(ab_i_1, pC + (offsetC+(i-1)*blk.inc2C)siz)
+                    vstore(ab_i_2, pC + (offsetC+(i-1)*blk.inc2C+4)siz)
+                    vstore(ab_i_3, pC + (offsetC+(i-1)*blk.inc2C+8)siz)
+                end
+            else
+                @nexprs 4 i -> begin
+                    vstore(ab_i_1, pAB + (i-1)blk.mr*siz)
+                    vstore(ab_i_2, pAB + ((i-1)blk.mr+4)*siz)
+                    vstore(ab_i_3, pAB + ((i-1)blk.mr+8)*siz)
+                end
+            end
+        else
+            fill!(blk.AB, zero(eltype(blk.AB)))
+            for k in 1:kc
+                for j in 1:blk.nr, i in 1:blk.mr
+                    blk.AB[i + (j-1)*blk.mr] += blk.Ac[offsetA+i] * blk.Bc[offsetB+j]
+                end
+                offsetA += blk.mr
+                offsetB += blk.nr
+            end
+            if loadC
+                for j in 1:blk.nr, i in 1:blk.mr
+                    blk.C[offsetC+(i-1)*blk.inc1C+(j-1)*blk.inc2C+1] += blk.AB[i + (j-1)*blk.mr]
+                end
+            end
+        end
+        return nothing
     end
 end
 
